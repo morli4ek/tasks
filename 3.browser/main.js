0d10b9e6a0a6c0c251f7c97173d1901bca8503d1
morli4ek <krimm4ik@gmail.com>
2022-03-15 16:41:13 +0200

    add browser task

---
+++ 3.browser/main.js
@@ -0,0 +1,10 @@
+const outputBlock = document.querySelector('.output');   
+const input = document.querySelector(".input")
+const infoBlock = document.querySelector(".info")
+const pagrafBlock = document.querySelector(".pagraf")
+const stringBlock =document.querySelector(".string")
+
+outputBlock.innerHTML = "hello world" //  write information
+infoBlock.innerHTML = "main info"
+pagrafBlock.innerHTML = "this is text"
+stringBlock.innerHTML = "string123"
--- README.md
+++ README.md
@@ -1,18 +1,6 @@
 # git commands
 
-add filse to index
-```
+
 git add -A
-```
-
-create commit
-
-```
 git commit -m "commit name"
-```
-
-push commit 
-
-```
 git push origin branchName
-```
