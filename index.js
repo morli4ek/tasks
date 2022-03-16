b5cc372110810be93a1f9bc934667fd4151fc040
Zhyvyj <zhyvyj.web@gmail.com>
2022-03-16 11:46:37 +0200

    task 4

---
+++ index.js
@@ -0,0 +1,8 @@
+const onInput = (value) => {
+    console.log(value)
+}
+
+const input = document.querySelector('input');
+const output = document.querySelector('.test');
+
+output.innerHTML = input.value
