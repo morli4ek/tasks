f1af5ca4ae9f66c74abe0115f023ff31be44eec4
morli4ek <krimm4ik@gmail.com>
2022-03-16 17:35:48 +0200

    add styles to task 4

--- 4.numbers-game/numbers.js
+++ 4.numbers-game/numbers.js
@@ -1,1 +1,19 @@
-console.log("test")
+const selectNumber = (number) => {
+    const selectedNumber = number;
+    console.log(selectedNumber)
+}
+
+const generatePlayNumber = () => {
+    const randomNumber =  Math.random()*10
+    return Math.round(randomNumber)
+}
+
+const play = () => {
+    console.log('play')
+     generatePlayNumber()
+}
+ 
+play()
+//  console.log(generatePlayNumber() )
+
+
