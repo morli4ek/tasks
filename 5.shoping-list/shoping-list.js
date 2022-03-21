74b536913965f5d97ede2bb6193aa6a1ab9966ac
Zhyvyj <zhyvyj.web@gmail.com>
2022-03-21 18:30:56 +0200

    start notes

--- 5.shoping-list/shoping-list.js
+++ 5.shoping-list/shoping-list.js
@@ -18,4 +18,21 @@ const addNewProduct = () => {
         productPriceInput.value = undefined;
     };
     console.log(productList);
+    createListItem(newProduct.name, newProduct.price)
 }
+
+const createListItem = (name, price) => {
+    const shopingList = document.querySelector('.shoping-list');
+    const shopungListItem = document.createElement("div");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "delete"
+
+    shopungListItem.classList.add('shoping-list-item');
+    shopungListItem.innerHTML = name + price;
+    const listItem = shopingList.appendChild(shopungListItem);
+    listItem.appendChild(deleteButton)
+}
+
+const deleteItem = (itemId) => {
+    
+} 
