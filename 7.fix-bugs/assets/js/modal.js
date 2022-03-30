b5cbb024fba0e3a778d44f7c3c7dd2768e5531b3
morli4ek <krimm4ik@gmail.com>
2022-03-30 20:22:34 +0300

    add basic logic to task 7

--- 7.fix-bugs/assets/js/modal.js
+++ 7.fix-bugs/assets/js/modal.js
@@ -1,1 +1,24 @@
+const modal = document.createElement("div");
+const deleteButton = document.createElement("button");
 
+const openModal = () => {
+    createModal();
+}
+
+const createModal = () => {
+    modal.classList.add("modal-window")
+    
+    //create delete button
+    deleteButton.classList.add("modal-delete-button");
+    deleteButton.innerHTML = "delete";
+    deleteButton.addEventListener("click", closeModal)
+
+    //append model to document
+    document.body.appendChild(modal);
+    modal.appendChild(deleteButton);
+}
+
+const closeModal = () => {
+    modal.removeChild(deleteButton)
+    document.body.removeChild(modal);
+}
