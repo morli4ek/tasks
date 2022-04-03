f71fbb09883373c5438640d69ee8dd1154b4c042
morli4ek <krimm4ik@gmail.com>
2022-04-03 18:35:47 +0300

    disabled logic for button

--- 9.slideShow/slider.js
+++ 9.slideShow/slider.js
@@ -2,25 +2,34 @@ const imageBlock = document.querySelector(".slide-img");
 const buttonLeft = document.querySelector(".left");
 const buttonRight = document.querySelector(".right");
 
+
 const slides = ['cat1.jpg' , 'cat2.jpg' , 'cat3.jpg' , 'cat4.jpg'];
 
 let currentSlideIndex = 0;
+
+buttonLeft.disabled = true;
 
 const changeImage = () => {
     imageBlock.src = "img/" + slides[currentSlideIndex];
 };
 
 const nextSlide = () => {
-    // const isLastSlide = currentSlideIndex < slides.length;
-    // if (isLastSlide) {
-    //     buttonRight.disabled = true;
-    // };
+    buttonLeft.disabled = false;
     currentSlideIndex = currentSlideIndex + 1;
+    const isLastSlide = currentSlideIndex === slides.length - 1;
+    if (isLastSlide) {
+        buttonRight.disabled = true;
+    } 
     changeImage();
     
 };
 
 const previousSlide = () => {
+    buttonRight.disabled = false;
     currentSlideIndex = currentSlideIndex - 1;
+    const isFirstSlide = currentSlideIndex === 0;
+    if (isFirstSlide) {
+        buttonLeft.disabled = true
+    }
     changeImage();
 };
